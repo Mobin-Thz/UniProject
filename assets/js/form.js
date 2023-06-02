d07f90c38a3ecc1111cb436aa5c28a4ed0bbb0f0
mobin-thz <mobintaherzadeh@gmail.com>
2023-06-02 12:32:36 +0330

    first commit

---
+++ assets/js/form.js
@@ -0,0 +1,11 @@
+const labels = document.querySelectorAll('.form-control label');
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerHTML
+    .split('')
+    .map(
+      (letter, i) =>
+        `<span style="transition-delay:${i * 75}ms">${letter}</span>`
+    )
+    .join('');
+});
